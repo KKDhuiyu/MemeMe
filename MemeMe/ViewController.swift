474a16196b04d896a201a7ef6c18e674dc7bb521
Huiyu Jia <hjia@dn-wlan-133-203.sns.otago.ac.nz>
2017-03-20 13:19:35 +1300

    MemeMe init

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -10,15 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count=0
+    @IBOutlet var label:UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    func incrementCount(){
+
+        self.count+=1;
+        self.label.text = "\(self.count)"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
